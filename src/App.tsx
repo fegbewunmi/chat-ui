99c5c9add4da2b27cef332b6663a49fde6bcf02f
Ebun Egbewunmi <ebunegbewunmi@gmail.com>
2024-12-07 10:18:09 -0600

    initial commit and auth ui

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import Auth from "./components/auth/Auth";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+const App = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <Auth />
+      </Container>
+    </ThemeProvider>
+  );
+};
+export default App;
